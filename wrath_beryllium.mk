7395c9d95db70580ba75989e7afee33d1ebdc592
PainKiller3 <ninadpatil100@gmail.com>
2020-08-03 22:55:46 +0530

    beryllium: Update Fingerprint To Coral August Security Patch
    Change-Id: I856aa14047d4f23d7aba73795f971171e0f87f13
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- wrath_beryllium.mk
+++ wrath_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.8.0.QEJMIXM release-keys" \
